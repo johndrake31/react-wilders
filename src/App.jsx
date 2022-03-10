162e70d33abc5f40060f47d0d32dda2023907f0f
Peter May <peter.may@winzana.com>
2022-03-10 17:39:23 +0100

    played with styledComp

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 //Components
 import Wilders from './components/Wilders';
-// import Home from './components/Home';
+import Home from './components/Home';
 // import AddWildersForm from './components/AddWilderForm'
-// import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-      <header>
-        <h1 className='text-center'>Wilders Book</h1>
-      </header>
-        <Wilders></Wilders>
-
-      
-      {/* <Header />
+      <Header />
       <Switch>
         <Route path='/' exact>
-          <Home />
-        </Route>
-        <Route path='/home' exact>
           <Home />
         </Route>
         <Route path='/wilders' exact>
           <Wilders />
         </Route>
-        <Route path='/wilders-form' exact>
-          <AddWildersForm />
-        </Route>
-      </Switch> */}
+      </Switch>
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './Header.module.css';
+import styledComponents from 'styled-components';
+
+const StyledLi = styledComponents.li`
+margin: 1rem 1rem;
+color: black;
+
+`;
+const StyledLiRed = styledComponents(StyledLi)`
+background-color: red !important;
+`;
 
 const Header = (props) => {
   return (
-    <header className={classes.header}>
-      <ul className='d-flex'>
-        <li>
+    <header>
+      <div className='container'>
+        <h1 className='text-center'>Wilders Book</h1>
+      </div>
+
+      <ul className={classes.flex}>
+        <StyledLi>
           <Link to='/'>Home</Link>
-        </li>
-        <li>
+        </StyledLi>
+        <StyledLi>
           <Link to='/wilders'>Wilders List</Link>
-        </li>
-        <li>
+        </StyledLi>
+        <StyledLi>
           <Link to='/wilders-form'>Add Wilders</Link>
-        </li>
+        </StyledLi>
       </ul>
     </header>
   );
