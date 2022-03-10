145e46cf44078d74bef6a2aec317282247b8ab60
Peter May <peter.may@winzana.com>
2022-03-10 10:25:03 +0100

    add useContext to Wilders comp and map data

--- src/components/Wilders.jsx
+++ src/components/Wilders.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import classes from './Home.module.css'
+import React, {useContext} from 'react';
+import WilderContext from '../context/WilderContext'; 
+import classes from './Wilders.module.css'
+
 const Wilders = (props) => {
+    const {wilders, setWilders} = useContext(WilderContext);
+
     return ( 
-    <>
-    <div>
-        <h2>WIlders List</h2>
+    <div className='container'>
+        <h2 className='text-dark'>WIlders List</h2>
+        {wilders && wilders.map((wilder)=>(
+            <div key={wilder._id}>
+                <p>{wilder.name}</p>
+                <p>{wilder.city}</p>
+            </div>
+        ))}
     </div>
-    </> 
+
     );
 }
  
--- src/context/WilderContext.jsx
+++ src/context/WilderContext.jsx
@@ -6,25 +6,28 @@ const WilderContext = createContext(null);
 
 export const WilderContextProvider = (props)=>{
     const [wilders, setWilders] = useState([{
-        "_id": "622721f5d76d0de75c85dcdb",
-        "name": "1st Wilder",
-        "city": "San Francisco",
-        "skills": [
+        _id: "622721f5d76d0de75c85dcdb",
+        name: "1st Wilder",
+        city: "San Francisco",
+        skills: [
             {
                 "title": "HTML",
                 "votes": 10,
-                "_id": "622721f5d76d0de75c85dcdc"
+                _id: "622721f5d76d0de75c85dcdc"
             },
             {
                 "title": "React",
                 "votes": 5,
-                "_id": "622721f5d76d0de75c85dcdd"
+                _id: "622721f5d76d0de75c85dcdd"
             }
         ],
         "__v": 0
     }])
-
-    return <WilderContext.Provider>{props.children}</WilderContext.Provider>
+    const contextValue = {
+        wilders: wilders,
+        setWilders: setWilders
+    }
+    return <WilderContext.Provider value={contextValue}>{props.children}</WilderContext.Provider>
 }
 
 export default WilderContext;
